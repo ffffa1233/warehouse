d0f1c02fa23c8a0bfd3262542aecaf0b65107cb1
sc.park <seongcheolpark@SeongCheolui-MacBook-Pro.local>
2023-03-11 16:10:32 +0900

    django app "home" 추가... 테스트용 텍스트만 들어가 있음

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
     path('frige/', include('frige.urls')),
     path('notice/', include('notice.urls')),
     #path('', include('frige.urls')),
